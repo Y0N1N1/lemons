3013357a00cce018796d4dca7c87504f1a0ad18d
Gabriel <73286903+Y0N1N1@users.noreply.github.com>
2021-01-20 19:48:01 +0000

    🎍 optims stuff #2

--- lemons/optimizer.py
+++ lemons/optimizer.py
@@ -138,56 +138,96 @@ class Optimizer:
       self.update_vector, self.opt_avg, self.update_vector_avg = -1, -1, 1
       self.optimizer = 0
       
-  def adadelta(self, gradient):
-    gradient_squared = (gradient)*(gradient)
-    self.opt_avg = ( self.decay_rate * self.opt_avg) + (gradient_squared * (1 - self.decay_rate))
-    self.update_vector_avg = (self.decay_rate * self.update_vector_avg) + (self.update_vector * (1 - self.decay_rate))
-    self.update_vector = -(((math.sqrt(self.update_vector_avg + self.fudge_factor)) / (math.sqrt(self.opt_avg + self.fudge_factor)))*gradient)
-    self.optimizer = self.optimizer + self.update_vector
-    return  self.optimizer
+    def comp(self, gradient):
+      gradient_squared = (gradient)*(gradient)
+      self.opt_avg = ( self.decay_rate * self.opt_avg) + (gradient_squared * (1 - self.decay_rate))
+      self.update_vector_avg = (self.decay_rate * self.update_vector_avg) + (self.update_vector * (1 - self.decay_rate))
+      self.update_vector = -(((math.sqrt(self.update_vector_avg + self.fudge_factor)) / (math.sqrt(self.opt_avg + self.fudge_factor)))*gradient)
+      self.optimizer = self.optimizer + self.update_vector
+      return  self.optimizer
 
 #______________________________________________________________________
 
-  def rms_prop(gradient, old_gradient_avg, decay_rate, old_optimizer, learning_rate, fudge_factor):
-    gradient_squared = (gradient * gradient)
-    gradient_avg = (decay_rate * old_gradient_avg) + ((1 - decay_rate) * gradient_squared)
-    optimizer = old_optimizer - ((learning_rate / math.sqrt(gradient_avg + fudge_factor)) * gradient)
-    return gradient_avg, optimizer
+  class rms_prop:
+    def __init__(self, learning_rate, fudge_factor, decay_rate):
+        self.learning_rate, self.fudge_factor, self.decay_rate = learning_rate, fudge_factor, decay_rate
+        self.gradient_avg = 1
+        self.optimizer = 0 
+      
+    def comp(self, gradient):
+      gradient_squared = (gradient * gradient)
+      self.gradient_avg = (self.decay_rate * self.gradient_avg) + ((1 - self.decay_rate) * gradient_squared)
+      self.optimizer = self.optimizer - ((self.learning_rate / math.sqrt(self.gradient_avg + self.fudge_factor)) * gradient)
+      return self.optimizer
 
 #______________________________________________________________________
 
-  def adam(gradient, decay_rate_one, decay_rate_two, learning_rate, fudge_factor, old_first_moment, old_second_moment, old_optimizer):
-    first_moment = (decay_rate_one * old_first_moment) + ((1 - decay_rate_one) * gradient) # usually: 0.9
-    second_moment = (decay_rate_two * old_second_moment) + ((1 - decay_rate_two) * (gradient * gradient)) # usually: 0.999
-    bias_corrected_first_moment = first_moment / (1 - decay_rate_one)
-    bias_corrected_second_moment = second_moment / (1 - decay_rate_two)
-    optimizer = old_optimizer - ((learning_rate / (math.sqrt(bias_corrected_second_moment) + fudge_factor)) * bias_corrected_first_moment)
-    return bias_corrected_first_moment, bias_corrected_second_moment, optimizer
+  class adam:
+    def __init__(self, learning_rate, fudge_factor,decay_rate_one, decay_rate_two):
+      self.decay_rate_one, self.decay_rate_two = decay_rate_one, decay_rate_two
+      self.learning_rate, self.fudge_factor = learning_rate, fudge_factor
+      self.optimizer = 0
+      self.first_moment = 1
+      self.second_moment = 1
+      
+    def comp(self, gradient):
+      self.first_moment = (self.decay_rate_one * self.first_moment) + ((1 - self.decay_rate_one) * gradient) # usually: 0.9
+      self.second_moment = (self.decay_rate_two * self.second_moment) + ((1 - self.decay_rate_two) * (gradient * gradient)) # usually: 0.999
+      self.first_moment = self.first_moment / (1 - self.decay_rate_one)
+      self.second_moment = self.second_moment / (1 - self.decay_rate_two)
+      self.optimizer = self.optimizer - ((self.learning_rate / (math.sqrt(self.second_moment) + self.fudge_factor)) * self.first_moment)
+      return self.optimizer
 
 #______________________________________________________________________
 
-  def adamax(gradient, decay_rate_one, decay_rate_two, learning_rate, old_first_moment, old_second_moment, old_optimizer):
-    first_moment = (decay_rate_one * old_first_moment) + ((1 - decay_rate_one) * gradient) # usually: 0.9
-    second_moment = max((decay_rate_two * old_second_moment) , abs(gradient)) # usually: 0.999
-    bias_corrected_first_moment = first_moment / (1 - decay_rate_one)
-    optimizer = old_optimizer - ((learning_rate / second_moment) * bias_corrected_first_moment)
-    return bias_corrected_first_moment, second_moment, optimizer
+  class adamax:
+    def __init__(self, learning_rate, decay_rate_one, decay_rate_two):
+      self.decay_rate_one, self.decay_rate_two = decay_rate_one, decay_rate_two
+      self.learning_rate = learning_rate
+      self.optimizer = 0
+      self.first_moment, self.second_moment = 1, 1
+      
+    def comp(self, gradient):
+      self.first_moment = (self.decay_rate_one * self.first_moment) + ((1 - self.decay_rate_one) * gradient) # usually: 0.9
+      self.second_moment = max((self.decay_rate_two * self.second_moment) , abs(gradient)) # usually: 0.999
+      self.first_moment = self.first_moment / (1 - self.decay_rate_one)
+      self.optimizer = self.optimizer - ((self.learning_rate / self.second_moment) * self.first_moment)
+      return  self.optimizer
 
 #______________________________________________________________________
 
-  def nadam(gradient, decay_rate_one, decay_rate_two, learning_rate, fudge_factor, old_first_moment, old_second_moment, old_optimizer):
-    first_moment = (decay_rate_one * old_first_moment) + ((1 - decay_rate_one) * gradient) # usually: 0.9
-    second_moment = (decay_rate_two * old_second_moment) + ((1 - decay_rate_two) * (gradient * gradient)) # usually: 0.999
-    bias_corrected_first_moment = first_moment / (1 - decay_rate_one)
-    bias_corrected_second_moment = second_moment / (1 - decay_rate_two)
-    optimizer = old_optimizer - ((learning_rate / (math.sqrt(bias_corrected_second_moment) + fudge_factor)) * ((decay_rate_one * bias_corrected_first_moment) + (((1 - decay_rate_one) * gradient) / (1 - decay_rate_one))))
-    return bias_corrected_first_moment, bias_corrected_second_moment, optimizer
+  class nadam:
+    def __init__(self, learning_rate, fudge_factor, decay_rate_one, decay_rate_two):
+      self.learning_rate, self.fudge_factor = learning_rate, fudge_factor
+      self.decay_rate_one, self.decay_rate_two = decay_rate_one, decay_rate_two
+      self.first_moment, self.second_moment = 1, 1
+      self.optimizer = 0
+    
+    def comp(self, gradient):
+      self.first_moment = (self.decay_rate_one * self.first_moment) + ((1 - self.decay_rate_one) * gradient) # usually: 0.9
+      self.second_moment = (self.decay_rate_two * self.second_moment) + ((1 - self.decay_rate_two) * (gradient * gradient)) # usually: 0.999
+      self.first_moment = self.first_moment / (1 - self.decay_rate_one)
+      self.second_moment = self.second_moment / (1 - self.decay_rate_two)
+      self.optimizer = self.optimizer - ((self.learning_rate / (math.sqrt(self.second_moment) + self.fudge_factor)) * ((self.decay_rate_one * self.first_moment) + (((1 - self.decay_rate_one) * gradient) / (1 - self.decay_rate_one))))
+      return self.optimizer
 
 #______________________________________________________________________
 
-  def amsgrad(gradient, decay_rate_one, decay_rate_two, old_first_moment, old_second_moment, old_bias_corrected_second_moment, old_optimizer, learning_rate, fudge_factor):
-    first_moment = (decay_rate_one * old_first_moment) + ((1 - decay_rate_one) * gradient)
-    second_moment =  (decay_rate_two * old_second_moment) + ((1 - decay_rate_two) * (gradient * gradient))
-    bias_corrected_second_moment = max(old_bias_corrected_second_moment, second_moment)
-    optimizer = old_optimizer - ((learning_rate / (sqrt(bias_corrected_second_moment) + fudge_factor)) * first_moment)
-    return first_moment, second_moment, bias_corrected_second_moment, optimizer
+  class amsgrad:
+    def __init__(self, learning_rate, fudge_factor, decay_rate_one, decay_rate_two):
+      self.learning_rate, self.fudge_factor = learning_rate, fudge_factor
+      self.decay_rate_one, self.decay_rate_two = decay_rate_one, decay_rate_two
+      self.first_moment, self.second_moment = 1, 1
+      self.optimizer = 0
+    
+    def comp(self, gradient):
+      self.first_moment = (self.decay_rate_one * self.first_moment) + ((1 - self.decay_rate_one) * gradient)
+      self.second_moment =  (self.decay_rate_two * self.second_moment) + ((1 - self.decay_rate_two) * (gradient * gradient))
+      self.second_moment = max(self.bias_second_moment, self.second_moment)
+      self.bias_second_moment = self.second_moment
+      self.optimizer = self.optimizer - ((self.learning_rate / (sqrt(self.second_moment) + self.fudge_factor)) * self.first_moment)
+      return self.optimizer
+
+#______________________________________________________________________
+ 
+#end
