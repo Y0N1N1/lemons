9b2aab4b206fe6102b402c3773eb16f62c2db7dd
Gabriel <73286903+Y0N1N1@users.noreply.github.com>
2021-01-21 10:54:35 +0000

    Update layer.py

--- lemons/layer.py
+++ lemons/layer.py
@@ -2,6 +2,12 @@ import math
 import os
 import random
 from random import random
+
+"""
+Layer ->
+  dense
+"""
+
 class Layer:
   class dense:
     def __init__(self, size, activation, bias=None):
@@ -11,15 +17,32 @@ class Layer:
       else:
         self.bias = bias
 
+#______________________________________________________________________
     
     def size(self):
       return self.size
 
+#______________________________________________________________________
+
     def activation(self):
       return self.activation.__name__
     
+#______________________________________________________________________
+
     def bias(self):
       return self.bias
 
-    def comp(self, wei):
+#______________________________________________________________________
+
+    def comp(self, weight_list, neuron_list):
+      tot = 0
+      for i in range weight_list:
+        x = i * neuron_list[i]
+        tot += x
+      tot += self.bias
+      tot = self.activation.comp(tot)
+      return tot
   
+#______________________________________________________________________
+
+#end
