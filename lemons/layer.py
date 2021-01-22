e86b162477d4a77fda535c2fd879fd7d2264f98b
Gabriel <73286903+Y0N1N1@users.noreply.github.com>
2021-01-22 14:41:48 +0000

    Update layer.py

--- lemons/layer.py
+++ lemons/layer.py
@@ -13,32 +13,143 @@ class Layer:
     def __init__(self, size, activation, bias=None):
       self.size, self.activation = size, activation
       if bias == None:
-        self.bias = - ((random() + random() + random()) * (random() + random() + random()))
+        self.bias = -random.randint(0,10)
       else:
         self.bias = bias
 
-#______________________________________________________________________
+#_________________________________
     
     def size(self):
       return self.size
 
-#______________________________________________________________________
-
+#_________________________________
+    
     def activation(self):
       return self.activation.__name__
+ 
+#_________________________________
     
-#______________________________________________________________________
-
     def bias(self):
       return self.bias
 
-#______________________________________________________________________
-
+#_________________________________
+     
     def comp(self, weight_list, neuron_list):
       tot = 0
       for i in range weight_list:
         x = i * neuron_list[i]
         tot += x
+      tot += self.bias
+      tot = self.activation.comp(tot)
+      return tot
+  
+#______________________________________________________________________
+
+  class average:
+    def __init__(self, size, activation, bias=None):
+      self.size, self.activation = size, activation
+      if bias == None:
+        self.bias = -random.randint(0,10)
+      else:
+        self.bias = bias
+
+#_________________________________
+    
+    def size(self):
+      return self.size
+
+#_________________________________
+    
+    def activation(self):
+      return self.activation.__name__
+ 
+#_________________________________
+    
+    def bias(self):
+      return self.bias
+
+#_________________________________
+    
+    def comp(self, weight_list, neuron_list):
+      tot = []
+      for i in range weight_list:
+        x = i * neuron_list[i]
+        tot.append(x)
+      tot = sum(tot) / len(tot)
+      tot += self.bias
+      tot = self.activation.comp(tot)
+      return tot
+  
+#______________________________________________________________________
+
+  class maximum:
+    def __init__(self, size, activation, bias=None):
+      self.size, self.activation = size, activation
+      if bias == None:
+        self.bias = -random.randint(0,10)
+      else:
+        self.bias = bias
+
+#_________________________________
+    
+    def size(self):
+      return self.size
+
+#_________________________________
+    
+    def activation(self):
+      return self.activation.__name__
+ 
+#_________________________________
+    
+    def bias(self):
+      return self.bias
+
+#_________________________________
+    
+    def comp(self, weight_list, neuron_list):
+      tot = []
+      for i in range weight_list:
+        x = i * neuron_list[i]
+        tot.append(x)
+      tot = max(tot)
+      tot += self.bias
+      tot = self.activation.comp(tot)
+      return tot
+  
+#______________________________________________________________________
+
+  class minimum:
+    def __init__(self, size, activation, bias=None):
+      self.size, self.activation = size, activation
+      if bias == None:
+        self.bias = -random.randint(0,10)
+      else:
+        self.bias = bias
+
+#_________________________________
+    
+    def size(self):
+      return self.size
+
+#_________________________________
+    
+    def activation(self):
+      return self.activation.__name__
+ 
+#_________________________________
+    
+    def bias(self):
+      return self.bias
+
+#_________________________________
+    
+    def comp(self, weight_list, neuron_list):
+      tot = []
+      for i in range weight_list:
+        x = i * neuron_list[i]
+        tot.append(x)
+      tot = min(tot)
       tot += self.bias
       tot = self.activation.comp(tot)
       return tot
