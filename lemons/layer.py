e4bbdfc16322a083c34c9e0a502572b7e051e08c
Gabriel <73286903+Y0N1N1@users.noreply.github.com>
2021-01-23 10:02:41 +0000

    Update layer.py

--- lemons/layer.py
+++ lemons/layer.py
@@ -6,6 +6,13 @@ from random import random
 """
 Layer ->
   dense
+  average
+  maximum
+  minimum
+  add
+  subtract
+  multiply
+  activation
 """
 
 class Layer:
@@ -36,7 +43,7 @@ class Layer:
      
     def comp(self, weight_list, neuron_list):
       tot = 0
-      for i in range weight_list:
+      for i in range(weight_list):
         x = i * neuron_list[i]
         tot += x
       tot += self.bias
@@ -72,7 +79,7 @@ class Layer:
     
     def comp(self, weight_list, neuron_list):
       tot = []
-      for i in range weight_list:
+      for i in range(weight_list):
         x = i * neuron_list[i]
         tot.append(x)
       tot = sum(tot) / len(tot)
@@ -109,7 +116,7 @@ class Layer:
     
     def comp(self, weight_list, neuron_list):
       tot = []
-      for i in range weight_list:
+      for i in range(weight_list):
         x = i * neuron_list[i]
         tot.append(x)
       tot = max(tot)
@@ -146,12 +153,143 @@ class Layer:
     
     def comp(self, weight_list, neuron_list):
       tot = []
-      for i in range weight_list:
+      for i in range(weight_list):
         x = i * neuron_list[i]
         tot.append(x)
       tot = min(tot)
       tot += self.bias
       tot = self.activation.comp(tot)
+      return tot
+  
+#______________________________________________________________________
+
+  class add:
+    def __init__(self, size, activation, bias=None):
+      self.size, self.activation = size, activation
+      if bias == None:
+        self.bias = -random.randint(0,10)
+      else:
+        self.bias = bias
+
+#_________________________________
+    
+    def size(self):
+      return self.size
+
+#_________________________________
+    
+    def activation(self):
+      return self.activation.__name__
+ 
+#_________________________________
+    
+    def bias(self):
+      return self.bias
+
+#_________________________________
+    
+    def comp(self, weight_list, neuron_list):
+      tot = 0
+      for i in range(neuron_list):
+        tot += i
+      tot += self.bias
+      tot = self.activation.comp(tot)
+      return tot
+  
+#______________________________________________________________________
+
+  class subtract:
+    def __init__(self, size, activation, bias=None):
+      self.size, self.activation = size, activation
+      if bias == None:
+        self.bias = -random.randint(0,10)
+      else:
+        self.bias = bias
+
+#_________________________________
+    
+    def size(self):
+      return self.size
+
+#_________________________________
+    
+    def activation(self):
+      return self.activation.__name__
+ 
+#_________________________________
+    
+    def bias(self):
+      return self.bias
+
+#_________________________________
+    
+    def comp(self, weight_list, neuron_list):
+      tot = 0
+      for i in range(neuron_list):
+        tot -= i
+      tot += self.bias
+      tot = self.activation.comp(tot)
+      return tot
+  
+#______________________________________________________________________
+
+  class multiply:
+    def __init__(self, size, activation, bias=None):
+      self.size, self.activation = size, activation
+      if bias == None:
+        self.bias = -random.randint(0,10)
+      else:
+        self.bias = bias
+
+#_________________________________
+    
+    def size(self):
+      return self.size
+
+#_________________________________
+    
+    def activation(self):
+      return self.activation.__name__
+ 
+#_________________________________
+    
+    def bias(self):
+      return self.bias
+
+#_________________________________
+    
+    def comp(self, weight_list, neuron_list):
+      tot = 1
+      for i in range(neuron_list):
+        tot *= i
+      tot += self.bias
+      tot = self.activation.comp(tot)
+      return tot
+  
+#______________________________________________________________________
+
+  class activation:
+    def __init__(self, size, activation):
+      self.size, self.activation = size, activation
+
+#_________________________________
+    
+    def size(self):
+      return self.size
+
+#_________________________________
+    
+    def activation(self):
+      return self.activation.__name__
+
+#_________________________________
+    
+    def comp(self, weight_list, neuron_list):
+      tot = []
+      for i in range(neuron_list):
+        x = self.activation.comp(i)
+        tot.append(x)
+      tot = sum(tot) / len(tot)
       return tot
   
 #______________________________________________________________________
