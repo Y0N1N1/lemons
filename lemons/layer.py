ccb2a9ae1d079555d094bc1731905b037d72b421
Gabriel <73286903+Y0N1N1@users.noreply.github.com>
2021-01-25 13:07:46 +0000

    Update layer.py

--- lemons/layer.py
+++ lemons/layer.py
@@ -17,13 +17,9 @@ Layer ->
 
 class Layer:
   class dense:
-    def __init__(self, size, activation, bias=None):
+    def __init__(self, size, activation):
       self.size, self.activation = size, activation
-      if bias == None:
-        self.bias = -random.randint(0,10)
-      else:
-        self.bias = bias
-
+      
 #_________________________________
     
     def size(self):
@@ -35,30 +31,21 @@ class Layer:
       return self.activation.__name__
  
 #_________________________________
-    
-    def bias(self):
-      return self.bias
-
-#_________________________________
      
-    def comp(self, weight_list, neuron_list):
+    def comp(self, weight_list, neuron_list, bias):
       tot = 0
       for i in range(weight_list):
         x = i * neuron_list[i]
         tot += x
-      tot += self.bias
+      tot += bias
       tot = self.activation.comp(tot)
       return tot
   
 #______________________________________________________________________
 
   class average:
-    def __init__(self, size, activation, bias=None):
+    def __init__(self, size, activation):
       self.size, self.activation = size, activation
-      if bias == None:
-        self.bias = -random.randint(0,10)
-      else:
-        self.bias = bias
 
 #_________________________________
     
@@ -71,31 +58,22 @@ class Layer:
       return self.activation.__name__
  
 #_________________________________
-    
-    def bias(self):
-      return self.bias
 
-#_________________________________
-    
-    def comp(self, weight_list, neuron_list):
+    def comp(self, weight_list, neuron_list, bias):
       tot = []
       for i in range(weight_list):
         x = i * neuron_list[i]
         tot.append(x)
       tot = sum(tot) / len(tot)
-      tot += self.bias
+      tot += bias
       tot = self.activation.comp(tot)
       return tot
   
 #______________________________________________________________________
 
   class maximum:
-    def __init__(self, size, activation, bias=None):
+    def __init__(self, size, activation):
       self.size, self.activation = size, activation
-      if bias == None:
-        self.bias = -random.randint(0,10)
-      else:
-        self.bias = bias
 
 #_________________________________
     
@@ -109,30 +87,21 @@ class Layer:
  
 #_________________________________
     
-    def bias(self):
-      return self.bias
-
-#_________________________________
-    
-    def comp(self, weight_list, neuron_list):
+    def comp(self, weight_list, neuron_list, bias):
       tot = []
       for i in range(weight_list):
         x = i * neuron_list[i]
         tot.append(x)
       tot = max(tot)
-      tot += self.bias
+      tot += bias
       tot = self.activation.comp(tot)
       return tot
   
 #______________________________________________________________________
 
   class minimum:
-    def __init__(self, size, activation, bias=None):
+    def __init__(self, size, activation):
       self.size, self.activation = size, activation
-      if bias == None:
-        self.bias = -random.randint(0,10)
-      else:
-        self.bias = bias
 
 #_________________________________
     
@@ -146,30 +115,21 @@ class Layer:
  
 #_________________________________
     
-    def bias(self):
-      return self.bias
-
-#_________________________________
-    
-    def comp(self, weight_list, neuron_list):
+    def comp(self, weight_list, neuron_list, bias):
       tot = []
       for i in range(weight_list):
         x = i * neuron_list[i]
         tot.append(x)
       tot = min(tot)
-      tot += self.bias
+      tot += bias
       tot = self.activation.comp(tot)
       return tot
   
 #______________________________________________________________________
 
   class add:
-    def __init__(self, size, activation, bias=None):
+    def __init__(self, size, activation):
       self.size, self.activation = size, activation
-      if bias == None:
-        self.bias = -random.randint(0,10)
-      else:
-        self.bias = bias
 
 #_________________________________
     
@@ -183,28 +143,19 @@ class Layer:
  
 #_________________________________
     
-    def bias(self):
-      return self.bias
-
-#_________________________________
-    
-    def comp(self, weight_list, neuron_list):
+    def comp(self, weight_list, neuron_list, bias):
       tot = 0
       for i in range(neuron_list):
         tot += i
-      tot += self.bias
+      tot += bias
       tot = self.activation.comp(tot)
       return tot
   
 #______________________________________________________________________
 
   class subtract:
-    def __init__(self, size, activation, bias=None):
+    def __init__(self, size, activation):
       self.size, self.activation = size, activation
-      if bias == None:
-        self.bias = -random.randint(0,10)
-      else:
-        self.bias = bias
 
 #_________________________________
     
@@ -218,28 +169,19 @@ class Layer:
  
 #_________________________________
     
-    def bias(self):
-      return self.bias
-
-#_________________________________
-    
-    def comp(self, weight_list, neuron_list):
+    def comp(self, weight_list, neuron_list, bias):
       tot = 0
       for i in range(neuron_list):
         tot -= i
-      tot += self.bias
+      tot += bias
       tot = self.activation.comp(tot)
       return tot
   
 #______________________________________________________________________
 
   class multiply:
-    def __init__(self, size, activation, bias=None):
+    def __init__(self, size, activation):
       self.size, self.activation = size, activation
-      if bias == None:
-        self.bias = -random.randint(0,10)
-      else:
-        self.bias = bias
 
 #_________________________________
     
@@ -253,16 +195,11 @@ class Layer:
  
 #_________________________________
     
-    def bias(self):
-      return self.bias
-
-#_________________________________
-    
-    def comp(self, weight_list, neuron_list):
+    def comp(self, weight_list, neuron_list, bias):
       tot = 1
       for i in range(neuron_list):
         tot *= i
-      tot += self.bias
+      tot += bias
       tot = self.activation.comp(tot)
       return tot
   
