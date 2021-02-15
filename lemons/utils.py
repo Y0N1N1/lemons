7767de8813757c639b143f58887794ebcce2aef8
Gabriel <73286903+Y0N1N1@users.noreply.github.com>
2021-02-15 20:41:57 +0000

    Update utils.py

--- lemons/utils.py
+++ lemons/utils.py
@@ -8,6 +8,7 @@ Utils ->
   list_prod
   point_distance
 """
+
 class Utils:
   def list_prod(list_to_prod):
     val = 1
@@ -20,14 +21,14 @@ class Utils:
   def point_distance(point1, point2):
     wrong_dim = Error("dimensions dont match")
     if len(point1) == len(point2):
-      self.point1 = point1
-      self.point2 = point2
-      self.dimensions = len(point1)
+      p1 = point1
+      p2 = point2
+      dimensions = len(point1)
     else:
       wrong_dim.custom()
     distance = 0.0
-    for i in range(self.dimensions):
-      sub = self.point1[i] - self.point2[i]
+    for i in range(dimensions):
+      sub = p1[i] - p2[i]
       distance += (sub * sub)
     return math.sqrt(distance)
     
