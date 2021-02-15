9a1bfa83625d94c33dcff2c435226dde9e2b3f8f
Gabriel <73286903+Y0N1N1@users.noreply.github.com>
2021-02-15 19:36:09 +0000

    ✅ CHECKED FOR LAUNCH ✅
    ✅ CHECKED FOR LAUNCH ✅

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
+import os
 from setuptools import setup
 
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+  long_description = f.read()
+
 setup(name='lemons',
-      version='0.0.1',
-      description='epic ml framework for the non-normies of ai.',
-      author='Y0N1N1 (gabriel)',
+      version='0.1.0',
+      description='dl framework for non-normies of ai, dependencies free',
+      author='Y0N1N1',
       license='MIT',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       packages = ['lemons'],
       classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License"
       ],
-      install_requires=['math','os'],
+      install_requires=[],
       python_requires='>=3.8',
       extras_require={
         'dev': [
             "math",
-            "os"
+            "os",
+            "random",
         ],
       },
       include_package_data=True)
