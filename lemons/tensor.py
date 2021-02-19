3935cb790d10ffaf7b5200ba38194c6018a4c586
Gabriel <73286903+Y0N1N1@users.noreply.github.com>
2021-02-19 16:20:07 +0000

    Update tensor.py

--- lemons/tensor.py
+++ lemons/tensor.py
@@ -74,12 +74,32 @@ class Tensor:
       self.data = dt
     shape.pop()
     self.data = Tensor.assign_zeros(shape=shape, data=self.data, first=false)
-    
-#_______________________________________________
-_______________________
 
-  def assign_identity(self, shape):
+#______________________________________________________________________
+
+  def assign_ones(self, shape=self.shape, data=[], first=True):
+    # shape must be m x n
+    self.data = data
+    s = math.prod(shape)
+    dt = []
+    for i in range(s):
+      if first:
+        self.data.append(1)
+      else:
+        x = self.data[0:(len(self.data)/s)]
+        dt.append(x)
+        for i in range(len(self.data)/s):
+          self.data.pop(0)
+    if not first:
+      self.data = dt
+    shape.pop()
+    self.data = Tensor.assign_zeros(shape=shape, data=self.data, first=false)
+    
+#______________________________________________________________________
+
+  def eye(self, shape):
     # shape must be n, such that actual shape is n x n
+    # eye makes a square matrix, diagonal with 1s
     self.data = []
     for i in range(shape):
       x = [0 for a in range(i)]
