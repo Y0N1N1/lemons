65e698c6d7204b90714c72b340edf99aeed9fe05
Gabriel <73286903+Y0N1N1@users.noreply.github.com>
2021-02-19 16:12:12 +0000

    Update tensor.py

--- lemons/tensor.py
+++ lemons/tensor.py
@@ -46,19 +46,37 @@ class Tensor:
   
 #______________________________________________________________________
 
+  @property
+  def dtype(self):
+    return "tensor"
+
+#______________________________________________________________________
+
   def assign(self, x):
     self.data = x.data
       
 #______________________________________________________________________
 
-  def assign_zeros(self, shape):
+  def assign_zeros(self, shape=self.shape, data=[], first=True):
     # shape must be m x n
-    self.data = []
-    for i in range(shape[0]):
-      x = [0 for a in range(shape[1])]
-      self.data.append(x)
-        
-#______________________________________________________________________
+    self.data = data
+    s = math.prod(shape)
+    dt = []
+    for i in range(s):
+      if first:
+        self.data.append(0)
+      else:
+        x = self.data[0:(len(self.data)/s)]
+        dt.append(x)
+        for i in range(len(self.data)/s):
+          self.data.pop(0)
+    if not first:
+      self.data = dt
+    shape.pop()
+    self.data = Tensor.assign_zeros(shape=shape, data=self.data, first=false)
+    
+#_______________________________________________
+_______________________
 
   def assign_identity(self, shape):
     # shape must be n, such that actual shape is n x n
