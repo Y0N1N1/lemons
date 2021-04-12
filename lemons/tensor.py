205204c73b5b229ae17f194abcd7cc737fa39564
Gabriel <73286903+Y0N1N1@users.noreply.github.com>
2021-04-12 19:20:39 +0100

    Update tensor.py

--- lemons/tensor.py
+++ lemons/tensor.py
@@ -1,3 +1,5 @@
+from collections.abc import Sequence
+import random from random
 
 class tensor:
   def __init__(self, empty=False, form=(0, 1), shape=(2), data=[1, 1], flatten=False, grad=False, **kwargs):
@@ -21,6 +23,9 @@ class tensor:
     ## input all args kwargs to functions
     ## build numerical as tensor
     ## return it
+    
+  def assign(self, d):
+    self.data = d.data
     
   def __repr__(self):
     return f"<Tensor {self.data} of form {self.form} of shape {self.shape}>"
@@ -72,5 +77,73 @@ class tensor:
     ##create flattened data
     t = tensor(form=(0, 1), shape=(len(flattened)), data=flattened, flatten=False, **kwargs)
     return t
-
-
+  
+  @staticmethod
+  def zeros(t=False, form=(1, 1), shape=(2, 2), **kwargs):
+    if te == False:
+     data = []
+     size = 1
+     for i in shape:
+       size *= i
+     for i in range(size):
+       data.append(0)
+     return tensor(form=form, shape=shape, data=data, flatten=True, **kwargs)
+    else:
+      data = []
+      size = 1
+      for i in te.shape:
+        size *= i
+      for i in range(size):
+        data.append(0)
+      return tensor(form=te.form, shape=te.shape, data=data, flatten=True, **kwargs)
+  
+  @staticmethod
+  def ones(t=False, form=(1, 1), shape=(2, 2), **kwargs):
+    if te == False:
+     data = []
+     size = 1
+     for i in shape:
+       size *= i
+     for i in range(size):
+       data.append(1)
+     return tensor(form=form, shape=shape, data=data, flatten=True, **kwargs)
+    else:
+      data = []
+      size = 1
+      for i in te.shape:
+        size *= i
+      for i in range(size):
+        data.append(1)
+      return tensor(form=te.form, shape=te.shape, data=data, flatten=True, **kwargs)
+  
+  @staticmethod
+  def randn(t=False, form=(1, 1), shape=(2, 2), **kwargs):
+    if te == False:
+     data = []
+     size = 1
+     for i in shape:
+       size *= i
+     for i in range(size):
+       data.append(random())
+     return tensor(form=form, shape=shape, data=data, flatten=True, **kwargs)
+    else:
+      data = []
+      size = 1
+      for i in te.shape:
+        size *= i
+      for i in range(size):
+        data.append(random())
+      return tensor(form=te.form, shape=te.shape, data=data, flatten=True, **kwargs)
+    
+  @staticmethod
+  def eye(d, **kwargs):  
+    data = []
+    for i in range(d):
+      x = []
+      for a in range(i):
+        x.append(0)
+      x.append(1)
+      for a in range(d-i-1):
+        x.append(0)
+      data.append(x)
+    return tensor(form=(1, 1), shape=(d, d), data=data, flatten=False, **kwargs)
