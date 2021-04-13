8dfb72fc530c506211176bb39120e483fc0d6e32
Gabriel <73286903+Y0N1N1@users.noreply.github.com>
2021-04-13 21:19:51 +0100

    Update math.py

--- lemons/utils/math.py
+++ lemons/utils/math.py
@@ -3,6 +3,19 @@ import numbers
 from lemons._error import *
 
 def log(obj, base):
+  """takes in an object, either tensor or number, and computes log for each number
+        
+        Parameters
+        ----------
+        obj : int/tensor, must
+          the object being passed
+        base : int, must
+          the base of the log
+        Raises
+        ------
+        numnortensor
+            if the obj is neither a num nor a tensor
+    """
   if obj isinstanceof tensor:
     data = obj.flatten.data
     new = []
@@ -15,9 +28,20 @@ def log(obj, base):
   elif obj isinstanceof numbers.Number:
     return math.log(obj, base)
   else:
-    raise intnortensor('neither a number nor a tensor')
+    raise numnortensor('neither a number nor a tensor')
 
 def ln(obj):
+  """takes in an object, either tensor or number, and computes ln for each number
+        
+        Parameters
+        ----------
+        obj : int/tensor, must
+          the object being passed
+        Raises
+        ------
+        numnortensor
+            if the obj is neither a num nor a tensor
+    """
   if obj isinstanceof tensor:
     data = obj.flatten.data
     new = []
@@ -30,9 +54,20 @@ def ln(obj):
   elif obj isinstanceof numbers.Number:
     return math.log(obj)
   else:
-    raise intnortensor('neither a number nor a tensor')
+    raise numnortensor('neither a number nor a tensor')
     
 def exp(obj):
+  """takes in an object, either tensor or number, and computes exp for each number
+        
+        Parameters
+        ----------
+        obj : int/tensor, must
+          the object being passed
+        Raises
+        ------
+        numnortensor
+            if the obj is neither a num nor a tensor
+    """
   if obj isinstanceof tensor:
     data = obj.flatten.data
     new = []
@@ -45,9 +80,20 @@ def exp(obj):
   elif obj isinstanceof numbers.Number:
     return math.exp(obj)
   else:
-    raise intnortensor('neither a number nor a tensor')
+    raise numnortensor('neither a number nor a tensor')
     
 def sin(obj):
+  """takes in an object, either tensor or number, and computes sin for each number
+        
+        Parameters
+        ----------
+        obj : int/tensor, must
+          the object being passed
+        Raises
+        ------
+        numnortensor
+            if the obj is neither a num nor a tensor
+    """
   if obj isinstanceof tensor:
     data = obj.flatten.data
     new = []
@@ -60,9 +106,20 @@ def sin(obj):
   elif obj isinstanceof numbers.Number:
     return math.sin(obj)
   else:
-    raise intnortensor('neither a number nor a tensor')
+    raise numnortensor('neither a number nor a tensor')
     
 def cos(obj):
+  """takes in an object, either tensor or number, and computes cos for each number
+        
+        Parameters
+        ----------
+        obj : int/tensor, must
+          the object being passed
+        Raises
+        ------
+        numnortensor
+            if the obj is neither a num nor a tensor
+    """
   if obj isinstanceof tensor:
     data = obj.flatten.data
     new = []
@@ -75,9 +132,20 @@ def cos(obj):
   elif obj isinstanceof numbers.Number:
     return math.cos(obj)
   else:
-    raise intnortensor('neither a number nor a tensor')
+    raise numnortensor('neither a number nor a tensor')
     
 def tan(obj):
+  """takes in an object, either tensor or number, and computes tan for each number
+        
+        Parameters
+        ----------
+        obj : int/tensor, must
+          the object being passed
+        Raises
+        ------
+        numnortensor
+            if the obj is neither a num nor a tensor
+    """
   if obj isinstanceof tensor:
     data = obj.flatten.data
     new = []
@@ -90,9 +158,20 @@ def tan(obj):
   elif obj isinstanceof numbers.Number:
     return math.tan(obj)
   else:
-    raise intnortensor('neither a number nor a tensor')
+    raise numnortensor('neither a number nor a tensor')
     
 def squareroot(obj):
+  """takes in an object, either tensor or number, and computes sqrt for each number
+        
+        Parameters
+        ----------
+        obj : int/tensor, must
+          the object being passed
+        Raises
+        ------
+        numnortensor
+            if the obj is neither a num nor a tensor
+    """
   if obj isinstanceof tensor:
     data = obj.flatten.data
     new = []
@@ -105,9 +184,20 @@ def squareroot(obj):
   elif obj isinstanceof numbers.Number:
     return math.sqrt(obj)
   else:
-    raise intnortensor('neither a number nor a tensor')
+    raise numnortensor('neither a number nor a tensor')
     
 def factorial(obj):
+  """takes in an object, either tensor or number, and computes factorial for each number
+        
+        Parameters
+        ----------
+        obj : int/tensor, must
+          the object being passed
+        Raises
+        ------
+        numnortensor
+            if the obj is neither a num nor a tensor
+    """
   if obj isinstanceof tensor:
     data = obj.flatten.data
     new = []
@@ -120,4 +210,4 @@ def factorial(obj):
   elif obj isinstanceof numbers.Number:
     return math.factorial(obj)
   else:
-    raise intnortensor('neither a number nor a tensor')
+    raise numnortensor('neither a number nor a tensor')
